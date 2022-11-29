b893d37be8da3322fbd454cad3fe6ba5c323a736
Samuel Newman <mozzius@protonmail.com>
2022-11-29 21:21:21 +0000

    switch to email provider

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,5 +1,5 @@
 import { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/email";
 
 import { prisma } from "@acme/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -8,9 +8,9 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
     // ...add more providers here
   ],
